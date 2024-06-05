9eddf0761c02dd5c72f8c0221ae32f9a7c983934
connerc11 <cccochrane1@gmail.com>
2024-06-05 18:44:09 -0400

    updating the side margin to get the application working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import './App.css';
+import gptLogo from './assets/chatgpt.svg'
+import addBtn from './assets/add-30.png'
+import msgIcon from './assets/message.svg'
+import home from './assets/home.svg'
+import saved from './assets/bookmark.svg'
+import rocket from './assets/rocket.svg'
 
 function App() {
   return (
     <div className="App">
       <div className='sidebar'>
       <div className='upperSide'>
-        <div className='upperSideTop'> <img src='span' alt = "" className='logo' /> <span className='brand'>ChatGpt</span> </div>
-        <button className='midBtn'><img src="" alt="" className='addBtn' /> New Chat</button>
+        <div className='upperSideTop'> <img src={gptLogo} alt = "" className='logo' /> <span className='brand'>ChatGpt</span> </div>
+        <button className='midBtn'><img src={addBtn} alt="" className='addBtn' /> New Chat</button>
         <div className='upperSideBottom'>
-          <button className='query' ><img src="" alt=""/> What is Programming </button>
-          <button className='query' ><img src="" alt=""/> What is Programming </button>
+          <button className='query' ><img src={msgIcon} alt="Query"/> What is Programming </button>
+          <button className='query' ><img src={msgIcon} alt="Query"/> How to use an API </button>
         
         </div>
       </div>
-      <div className='lowerSide'></div>
+      <div className='lowerSide'>
+      <button className='listItems'><img src={home} alt="" className='listItemsImg' /> Home</button>
+      <button className='listItems'><img src={saved} alt="" className='listItemsImg' /> Save </button>
+      <button className='listItems'><img src={rocket} alt="" className='listItemsImg' /> Save </button>
+
+
+      </div>
     </div>
     <div className='main'> 
 
